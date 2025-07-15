7f29a033100927dd88327f6b5ce8f98590ef6571
Wesley <wesley.tech.samp@gmail.com>
2025-07-15 14:25:11 -0300

    Update

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,17 +1,21 @@
-//apontar para o título HTML h1
-let titulo = document.querySelector('h1');
+let numeroSecreto = gerarNumeroAleatorio();
 
-//pegar dentro do html a variável.innerHTML
-titulo.innerHTML = 'Jogo do Número Secreto';
+//melhor prática para o código.
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
-//apontar para o Parágrafo HTML p
-let paragrafo = document.querySelector('p');
-
-//pegar dentro do html a variável.innerHTML
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
-
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');//executando a função
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');//executando a função
 
 //função, criando uma funcionalidade para verificar o chute. Determinar alguma ação dentro do programa.
 function verificarChute() {
-    console.log('Olá')
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+//função para gerar o número aleatório
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 50 + 1);
 }
