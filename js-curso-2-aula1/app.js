79316172dc224d1e18cc2b8ae50b8646827e297d
Wesley <wesley.tech.samp@gmail.com>
2025-07-15 12:32:18 -0300

    Update

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,17 @@
+//apontar para o título HTML h1
+let titulo = document.querySelector('h1');
+
+//pegar dentro do html a variável.innerHTML
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+//apontar para o Parágrafo HTML p
+let paragrafo = document.querySelector('p');
+
+//pegar dentro do html a variável.innerHTML
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+
+
+//função, criando uma funcionalidade para verificar o chute. Determinar alguma ação dentro do programa.
+function verificarChute() {
+    console.log('Olá')
+}
