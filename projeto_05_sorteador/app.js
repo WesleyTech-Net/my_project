c420265d29719c85a9bd58f8600dd55288d87f9b
Wesley <wesley.tech.samp@gmail.com>
2025-07-22 21:52:32 -0300

    Update Sorteador de Números

--- projeto_05_sorteador/app.js
+++ projeto_05_sorteador/app.js
@@ -1,5 +1,7 @@
 
+let numerosSorteados = [];
 
+// função para pegar os valores dentro do input do html
 function limiteDeNumeros(){
     let numeroMinimo = parseInt(document.querySelector('#min-number').value);
     let numeroMaximo = parseInt(document.querySelector('#max-number').value);
@@ -20,24 +22,45 @@ function limiteDeNumeros(){
 }
 
 
-
+//função para sortear os números
 function sortearNumero(){
+    // chama a função limiteDeNumeros para obter os limites
     const limites = limiteDeNumeros();
 
+    //caso os limites sejam inválidos (null), a função para aqui
     if(!limites){
         return;
     }
 
+    //desestrutura os valores minimo e maximo do array retornado da funcao limiteDeNumeros
     const [minimo, maximo] = limites;
-
     console.log([minimo, maximo]);
 
-    const sorteado = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+    //calcula o total de possibilidades de números entre minimo e maximo
+    const totalDePossibilidades = maximo - minimo + 1;
 
-    document.getElementById('resultado').textContent = `NÚMERO SORTEADO: ${sorteado}`;
+    //verifica se todos os números já foram sorteados
+
+    if(numerosSorteados.length >= totalDePossibilidades){
+        alert('Todos os números já foram sorteados!'); //alerta o usuário  
+        return;
+    }
+
+    let sorteado;
+    // loop que continua até encontrar um número que nao foi sorteado
+    do{
+        sorteado = Math.floor(Math.random() * totalDePossibilidades) + minimo;
+
+    } while(numerosSorteados.includes(sorteado)); //Repete se o número já estiver no array
+
+    //adiciona o número sorteado ao array de já sorteados
+    numerosSorteados.push(sorteado);
     
+    //exibe o número sorteado na tela
+    document.getElementById('resultado').textContent = `NÚMERO SORTEADO: ${sorteado}`;
 }
 
+//função para limpar todos os campos de exebição
 function limparDados(){
     let numeroMinimo = document.querySelector('#min-number');
     numeroMinimo.value = '';
